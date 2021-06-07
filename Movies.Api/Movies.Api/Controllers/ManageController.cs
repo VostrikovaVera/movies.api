6a7142623d44da5f2133dffe290bca4830ba989d
Vira Vostrikova <vvostrikova@csod.com>
2021-06-07 22:39:57 +0300

    Initial commit

---
+++ Movies.Api/Movies.Api/Controllers/ManageController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Movies.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ManageController : ControllerBase
+    {
+        private readonly ILogger<ManageController> _logger;
+
+        public ManageController(ILogger<ManageController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation("Processing request GetMovies");
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
